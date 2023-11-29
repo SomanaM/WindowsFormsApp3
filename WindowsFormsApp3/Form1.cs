15f2dda099ebe523160f8ad370ec343b7540940c
DELL <DELL@DESKTOP-7Q6IETL>
2023-11-29 22:32:53 +0500

    Add project files.

---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "admin" && textBox2.Text == "pass123")
+            {
+                MessageBox.Show("Login Successfull!!!");
+                Form5 F5 = new Form5();
+                F5.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password");
+                textBox1.Text = string.Empty;
+                textBox2.Text = string.Empty;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            textBox2.Text = string.Empty;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form1 F1 = new Form1();
+            F1.Show();
+            this.Hide();
+        }
+    }
+}
